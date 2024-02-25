d1dccf85eef1ee9f1a04b9544e3917a3f6c4cc8b
GaboJohn <johngabotsalelwe@gmail.com>
2024-02-25 05:00:34 -0800

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 #Puppet to make changes to our configuration file
 
-file { 'etc/ssh/ssh_config':
+file { 'etc/ssh/ssh_cofig':
         ensure => present,
 content =>"
+
          #SSH client configuration
          host*
          IdentityFile ~/.ssh/school
          PasswordAuthentication no
 	 ",
+
 }
