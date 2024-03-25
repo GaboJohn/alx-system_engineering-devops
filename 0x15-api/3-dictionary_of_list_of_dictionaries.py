7cc5c40d7218ebd851e631121df513081d886ce4
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-25 12:43:44 -0700

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,53 +1,37 @@
 #!/usr/bin/python3
 """
-Script to gather data from an API and export TODO list progress for all employees in JSON format.
+Script to gather data from an API and export TODO list
+progress for all employees in JSON format.
 """
 
 import json
 import requests
 
-def gather_data():
-    """
-    Function to gather TODO list progress for all employees
-    """
 
-    # API URL
-    url = "https://jsonplaceholder.typicode.com"
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/users"
 
-    # Getting all users
-    users_response = requests.get(url + "/users")
-    users_data = users_response.json()
+    resp = requests.get(url)
+    Users = resp.json()
 
-    # Dictionary to store data for all employees
-    all_employees_data = {}
+    users_dict = {}
+    for user in Users:
+        USER_ID = user.get('id')
+        USERNAME = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(USER_ID)
+        url = url + '/todos/'
+        resp = requests.get(url)
 
-    for user in users_data:
-        user_id = user.get('id')
-        username = user.get('username')
-
-        # Getting TODO list for each user
-        todos_response = requests.get(url + "/todos?userId={}".format(user_id))
-        todos_data = todos_response.json()
-
-        # Formatting data for the user
-        user_tasks = []
-        for task in todos_data:
-            user_tasks.append({
-                "username": username,
-                "task": task.get('title'),
-                "completed": task.get('completed')
+        tasks = resp.json()
+        users_dict[USER_ID] = []
+        for task in tasks:
+            TASK_COMPLETED_STATUS = task.get('completed')
+            TASK_TITLE = task.get('title')
+            users_dict[USER_ID].append({
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
             })
-
-        # Adding user tasks to the dictionary
-        all_employees_data[user_id] = user_tasks
-
-    # Writing data to JSON file
-    json_filename = "todo_all_employees.json"
-    with open(json_filename, 'w') as jsonfile:
-        json.dump(all_employees_data, jsonfile, indent=4)
-
-    print("Data exported to {}".format(json_filename))
-
-
-if __name__ == "__main__":
-    gather_data()
+            """A little Something"""
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(users_dict, f)
