fd60430e77acecaa2487e790f2df06852407ec14
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-25 12:39:22 -0700

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,56 +1,33 @@
 #!/usr/bin/python3
 """
-Script to gather data from an API and display TODO list progress for a given employee ID.
+Script to gather data from an API and display TODO
+list progress for a given employee ID.
 """
 
 import json
 import requests
 import sys
 
-def gather_data(employee_id):
-    """
-    Function to gather TODO list progress for a given employee ID
-    """
 
-    # API URL
-    url = "https://jsonplaceholder.typicode.com"
+if __name__ == '__main__':
+    USER_ID = sys.argv[1]
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    res = requests.get(url_to_user)
+    """Documentation"""
+    USERNAME = res.json().get('username')
+    """Documentation"""
+    url_to_task = url_to_user + '/todos'
+    res = requests.get(url_to_task)
+    tasks = res.json()
 
-    # Getting user information
-    user_response = requests.get(url + "/users/{}".format(employee_id))
-    user_data = user_response.json()
-    user_id = user_data.get('id')
-    username = user_data.get('username')
-
-    # Getting TODO list
-    todos_response = requests.get(url + "/todos?userId={}".format(employee_id))
-    todos_data = todos_response.json()
-
-    # Formatting data
-    formatted_data = {"USER_ID": []}
-    for task in todos_data:
-        formatted_data["USER_ID"].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
-
-    # Writing data to JSON file
-    json_filename = "{}.json".format(user_id)
-    with open(json_filename, 'w') as jsonfile:
-        json.dump(formatted_data, jsonfile, indent=4)
-
-    print("Data exported to {}".format(json_filename))
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
-
-    gather_data(employee_id)
+    dict_data = {USER_ID: []}
+    for task in tasks:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        dict_data[USER_ID].append({
+                                  "task": TASK_TITLE,
+                                  "completed": TASK_COMPLETED_STATUS,
+                                  "username": USERNAME})
+    """print(dict_data)"""
+    with open('{}.json'.format(USER_ID), 'w') as f:
+        json.dump(dict_data, f)
